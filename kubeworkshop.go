eb5c181b4a644d3b710355283fe7c83e76db5393
Lasse Osterild <lasse@oesterild.dk>
2019-03-21 10:53:54 +0100

    add hostname

--- kubeworkshop.go
+++ kubeworkshop.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"time"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "-=- Kubernetes -=- 21 march 2019")
-		fmt.Fprintf(w, "%s", time.Now())
+		fmt.Fprintf(w, "%s\n", time.Now())
+		fmt.Fprintf(w, " -- hostname: %s", name)
 	})
 
 	//fs := http.FileServer(http.Dir("static/"))
